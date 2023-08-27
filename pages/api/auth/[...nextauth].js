d34d98d60355c4aab389585aa38dc6c4d73282e1
Arash Zarei <arashzarei.js@gmail.com>
2023-08-27 13:40:54 +0330

    create signin api usin next auth

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,36 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import connectDB from "@/utils/connectDB";
+import User from "@/models/User";
+import { verifyPassword } from "@/utils/auth";
+
+const authOptions = {
+  session: { strategy: "jwt" },
+  providers: [
+    CredentialsProvider({
+      async authorize(credentials, req) {
+        const { email, password } = credentials;
+        try {
+          await connectDB();
+        } catch (error) {
+          throw new Error("Can Not Connect To Data Base");
+        }
+
+        if (!email || !password) throw new Error("Invalid Data");
+
+        const user = await User.findOne({ email });
+
+        if (!user) throw new Error("User Dosn't Exist");
+
+        const isValid = await verifyPassword(password, user.password);
+        
+        if (!isValid) throw new Error("Email or Password is incorrect");
+
+        return { email };
+      },
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
