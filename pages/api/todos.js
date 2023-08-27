21b719685373b362d7640cb31b64fcc0c70c2265
Arash Zarei <arashzarei.js@gmail.com>
2023-08-27 14:37:38 +0330

    create api for add todo

---
+++ pages/api/todos.js
@@ -0,0 +1,46 @@
+import User from "@/models/User";
+import connectDB from "@/utils/connectDB";
+import { getSession } from "next-auth/react";
+
+const handler = async (req, res) => {
+  try {
+    await connectDB();
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ status: "failed", message: "Can Not Connect To Data Base" });
+  }
+
+  const session = await getSession({ req });
+
+  if (!session) {
+    return res
+      .status(401)
+      .json({ status: "failed", message: "You are not logged in!" });
+  }
+
+  const user = await User.findOne({ email: session.user.email });
+
+  if (!user) {
+    return res
+      .status(404)
+      .json({ status: "failed", message: "User doesn't exsit!" });
+  }
+
+  if (req.method === "POST") {
+    const { title, status, descriptions } = req.body;
+
+    if (!title || !status || !descriptions) {
+      return res
+        .status(422)
+        .json({ status: "failed", message: "Invaild data!" });
+    }
+
+    user.todos.push({ title, status, descriptions });
+    user.save()
+
+    res.status(201).json({ status: "success", message: "Todo created!" });
+  }
+};
+
+export default handler;
