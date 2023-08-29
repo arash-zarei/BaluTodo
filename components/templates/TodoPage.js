d821adf059a3f8ff7c15b816ac2e08feb7d942e6
Arash Zarei <arashzarei.js@gmail.com>
2023-08-29 17:51:08 +0330

    add edit button in todo page

--- components/templates/TodoPage.js
+++ components/templates/TodoPage.js
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import React from "react";
+import { MdOutlineModeEditOutline } from "react-icons/md";
 
 const TodoPage = ({ todo }) => {
   const { title, status, descriptions } = todo;
   return (
     <div className="w-full p-2 mt-4 flex justify-center items-center">
       <div className="w-[80%] border border-gray-400 rounded-lg p-5">
+      <Link href={`/edit/${todo._id}`}><MdOutlineModeEditOutline /></Link>
         <h1 className="text-2xl text-center text-blue-700">{title}</h1>
         <p
           className={`py-1 px-3 ${status === "todo" && "bg-orange-500"} ${
@@ -15,7 +18,7 @@ const TodoPage = ({ todo }) => {
         >
           {status}
         </p>
-        <textarea value={descriptions} className="w-full border border-gray-400 mt-5 px-2 py-4 rounded-xl leading-7 resize-none" rows="10"></textarea>
+        <textarea value={descriptions} disabled className="w-full border border-gray-400 mt-5 px-2 py-4 rounded-xl leading-7 resize-none" rows="10"></textarea>
       </div>
     </div>
   );
