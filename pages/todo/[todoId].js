9850ac241042cd6128489f3a51ccf8f9f3055210
Arash Zarei <arashzarei.js@gmail.com>
2023-08-30 14:23:39 +0330

    edit to get data

--- pages/todo/[todoId].js
+++ pages/todo/[todoId].js
@@ -1,26 +1,22 @@
 import User from "@/models/User";
 import TodoPage from "@/templates/TodoPage";
 import { getSession } from "next-auth/react";
-import { useRouter } from "next/router";
 
 const TodoDetails = ({ data }) => {
-    const router = useRouter()
-    const { todoId } = router.query
-    const todo = data.find(todo => todo._id === todoId)
-
-    return <TodoPage todo={todo} />;
+  return <TodoPage todo={data} />;
 };
 
 export default TodoDetails;
 
 export async function getServerSideProps(context) {
   const req = context.req;
+  const id = context.query.todoId;
   const session = await getSession({ req });
 
   const user = await User.findOne({ email: session.user.email });
-  
+  const todo = user.todos.find((todo) => todo._id.toString() === id);
 
   return {
-    props: {data: JSON.parse(JSON.stringify(user.todos))},
+    props: { data: JSON.parse(JSON.stringify(todo)) },
   };
 }
