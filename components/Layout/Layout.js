d840afb843ee6b2801c59b93895fbb620e34871d
ArashZarei <99428631+arashzareiJS@users.noreply.github.com>
2023-08-31 05:29:47 +0330

    Update Layout.js

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -40,7 +40,7 @@ const Layout = ({ children }) => {
           isOpen ? "left-[0%]" : "left-[-70%] sm:left-[-50%]"
         } lg:relative lg:w-[20%] lg:left-0 bg-slate-100 px-4 py-2 min-h-screen transition-all`}
       >
-        <div className="py-1 px-3 absolute bg-slate-100 rounded-xl top-12 right-[-35px] lg:hidden">
+        <div className="py-1 px-3 absolute bg-slate-100 rounded-xl top-12 right-[-35px] z-50 lg:hidden">
           {isOpen ? (
             <AiOutlineClose
               onClick={() => setIsOpen(false)}
