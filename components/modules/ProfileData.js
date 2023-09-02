0e4e69ee23b830a5d1f68229c3013e1cfd8a1a7b
ArashZarei <99428631+arashzareiJS@users.noreply.github.com>
2023-09-02 13:52:08 +0330

    Update ProfileData.js

--- components/modules/ProfileData.js
+++ components/modules/ProfileData.js
@@ -4,15 +4,15 @@ const ProfileData = ({ data }) => {
   const { name, lastName, email } = data;
   return (
     <div className="mt-8">
-      <div className="flex items-center gap-2 mt-3 bg-blue-300 rounded-md p-2">
+      <div className="flex items-center overflow-x-auto gap-2 mt-3 bg-blue-300 rounded-md p-2">
         <span className="text-gray-500">Name:</span>
         <p>{name}</p>
       </div>
-      <div className="flex items-center gap-2 mt-3 bg-blue-300 rounded-md p-2">
+      <div className="flex items-center overflow-x-auto gap-2 mt-3 bg-blue-300 rounded-md p-2">
         <span className="text-gray-500">Last Name:</span>
         <p>{lastName}</p>
       </div>
-      <div className="flex items-center gap-2 mt-3 bg-blue-300 rounded-md p-2">
+      <div className="flex items-center overflow-x-auto gap-2 mt-3 bg-blue-300 rounded-md p-2">
         <span className="text-gray-500">Email:</span>
         <p>{email}</p>
       </div>
